91ac4ae00861d1eb87d36956f44145fcf5ef40d7
Philipp Losbichler <philipp.losbichler@agravity.io>
2022-05-17 14:19:49 +0200

    Add Flight components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppComponent } from "./app.component";
+import { AppComponent } from './app.component';
+import { FlightModule } from './flight/flight.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FLIGHT_ROUTES } from './flight/flight.routes';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-  bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([...FLIGHT_ROUTES]),
+    FlightModule,
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
